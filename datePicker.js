788bf642402c707fdf859a2baf7452e638604111
Jay Poojara <jaypoojara007@gmail.com>
2020-04-15 19:03:42 +0530

    Error resolved

--- datePicker.js
+++ datePicker.js
@@ -201,6 +201,7 @@ class DatePicker extends Component {
     //date = date || new Date();
     let _this = this;
     let options = _.clone(this.props);
+    delete TouchableComponent;
     let date = this.state.date;
     // console.log({ date: moment(date) });
     if (options.mode == "time") {
